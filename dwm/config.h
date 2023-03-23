3ff49db3c69d47eb905640ddfb649c8f3099a19d
Nicholas <nhe@air.net.au>
2023-03-23 21:46:00 +1100

    Attachbelow patch

--- dwm/config.h
+++ dwm/config.h
@@ -6,6 +6,7 @@ static const unsigned int gappx     = 8;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int attachbelow	    = 1;	/* 1 means attach after the currently active window */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
