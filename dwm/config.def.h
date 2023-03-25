2900e19487223f78bf89444d68c316b69c1db4c8
Nicholas <nhe@air.net.au>
2023-03-25 17:18:53 +1100

    Add movestack patch to emulate other WMs

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -73,17 +73,26 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *roficmd[] = { "rofi", "-show", "drun", "-show-icons", NULL };
+static const char *rofiemoji[] = { "rofi", "-show", "emoji", "-show-icons", NULL };
+static const char *rofipass[] = { "rofi-pass", NULL };
 static const char *termcmd[]  = { "st", NULL };
+#include movestack.c
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_z,      spawn,          {.v = roficmd } },
+	{ MODKEY,                       XK_e,      spawn,          {.v = rofiemoji } },
+	{ MODKEY,                       XK_m,      spawn,          {.v = rofipass } },
 	{ MODKEY,	                XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,	                XK_x,      spawn,          {.v = rofibrowser } },
+	{ MODKEY,	                XK_Return, spawn,          {.v = filemanager } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
+//	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+//	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
